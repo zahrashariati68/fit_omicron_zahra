61accc0cefba105bbf5e4ee1bfcbbf3fec3843d5
zahrashariati68 <purpledreamS1!>
2025-03-20 18:32:42 +0100

    20250320

---
+++ src/20250320/ExCircularCalculation/ExCircularCalculation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExCircularCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double diameter = 15.12;
+            double circumference = 0.0;
+            circumference = diameter * Math.PI;
+            Console.WriteLine("the circumference is: "+circumference);
+            Console.ReadKey();
+        }
+    }
+}
