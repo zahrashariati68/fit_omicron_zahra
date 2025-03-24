3f58b5bf65ad5790b5e3319510b49395d7aa65cb
zahrashariati68 <zahrashariati68@gmail.com>
2025-03-24 19:57:42 +0100

    Decisions_if_else

---
+++ src/20250324/Decisions/Decisions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number =5;
+            if (number > 0 && number < 10)
+            {
+                Console.WriteLine("Number is out of range");
+            }
+            if (number < 0 || number > 10)
+            {
+                Console.WriteLine("Number is out of range");
+            }
+            if (number == 4)
+            {
+                Console.WriteLine("Number is 4");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 4");
+            }
+        }
+    }
+}
