0241544b9caeac6a5549d1816d3e6fd9c9902846
zahrashariati68 <zahrashariati68@gmail.com>
2025-03-27 19:56:02 +0100

    Basics about iterations

---
+++ src/20250327/Iterations/Iterations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Step: "+ i);
+            }
+        }
+    }
+}
