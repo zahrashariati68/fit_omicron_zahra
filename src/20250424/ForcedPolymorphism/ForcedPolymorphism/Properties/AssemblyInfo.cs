e1848498f0445778ec0b057d4fd81d92f70a44dd
zahrashariati68 <zahrashariati68@gmail.com>
2025-04-24 19:20:45 +0200

    Forced Polymorphism
    Forced Polymorphism

---
+++ src/20250424/ForcedPolymorphism/ForcedPolymorphism/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ForcedPolymorphism")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ForcedPolymorphism")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f6700b22-b59e-4e35-b6a2-06c425600732")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/20250424/ForcedPolymorphism/ForcedPolymorphism/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcedPolymorphism
+{
+    class Rectangle : IShape
+    {
+        private string _name;
+        private int _vertices;
+        public string Name { get { return _name; } }
+
+        public int Vertices { get { return _vertices; } }
+
+        public Rectangle(string rectangleName)
+        {
+            _name = "Rectangle" + rectangleName;
+            _vertices = 4;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"Drawing a {_name} with {_vertices} vertices.");
+        }
+    }
+}
